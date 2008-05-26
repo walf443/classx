2b6d048a469850308050e9d7188f834a76858657
Keiji, Yoshimi <walf443@gmail dot com>
2008-05-27 01:26:48 +0900

    Added spec.

--- spec/classx_spec.rb
+++ spec/classx_spec.rb
@@ -27,6 +27,23 @@ describe ClassX do
       end
     end
 
+    describe 'without any option' do
+        before do
+          @class = Class.new(ClassX)
+          @class.class_eval do
+            has :x
+          end
+        end
+
+        it 'should required :x on initialize' do
+          lambda { @class.new }.should raise_error(ClassX::AttrRequiredError)
+        end
+
+        it 'should define #x= private method to class' do
+          @class.private_instance_methods.map {|meth| meth.to_s }.should be_include("x=")
+        end
+    end
+
     describe 'with :is option' do
       describe 'when you specify :ro for attribute' do
         before do
