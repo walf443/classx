0b58fc18a0ea81803c284df6243d0086f15809ba
Keiji, Yoshimi <walf443@gmail.com>
2008-08-20 08:03:11 +0900

    change to private many method and tweak method define order.

--- lib/classx/attribute.rb
+++ lib/classx/attribute.rb
@@ -3,10 +3,6 @@ class ClassX
     def self.create args
       klass = Class.new
       klass.class_eval do
-        # args.each do |key, val|
-        #   key = key.to_s
-        #   include "ClassX::Attribute::#{key.capitalize}"
-        # end
         
         # XXX: Hack for defining class method for klass
         mod = Module.new
@@ -25,31 +21,40 @@ class ClassX
           @data = nil
         end
 
+        # description for attribute
+        define_method :desc do
+          args[:desc] 
+        end
+
         define_method :get do
           @data ||= default
         end
 
-        # for extending attribute point.
-        if args[:include]
-          case args[:include]
-          when Array
-            args[:include].each do |mod|
-              self.__send__ :include, mod
-            end
+        # default paramater for attribute.
+        # if default is Proc, run Proc every time in instanciate.
+        define_method :default do
+          case args[:default]
+          when Proc
+            args[:default].call(@parent)
           else
-            self.__send__ :include, args[:include]
+            args[:default]
           end
         end
 
-        if args[:extend]
-          case args[:extend]
-          when Array
-            args[:extend].each do |mod|
-              self.__send__ :extend, mod
-            end
-          else
-            self.__send__ :extend, args[:extend]
-          end
+        # when this option specify true, not raise error in #initialize without value.
+        define_method :optional? do
+          return args[:optional]
+        end
+
+        # when it lazy option specified, it will not be initialized when #initialize.
+        define_method :lazy? do
+          return args[:lazy]
+        end
+
+        define_method :set do |val|
+          val = coerce(val)
+          raise ClassX::InvalidAttrArgument unless validate? val
+          @data = val
         end
 
         # you specify type changing rule with :coerce option.
@@ -111,32 +116,6 @@ class ClassX
           end
         end
 
-        # description for attribute
-        define_method :desc do
-          args[:desc] 
-        end
-
-        # default paramater for attribute.
-        # if default is Proc, run Proc every time in instanciate.
-        define_method :default do
-          case args[:default]
-          when Proc
-            args[:default].call(@parent)
-          else
-            args[:default]
-          end
-        end
-
-        # when it lazy option specified, it will not be initialized when #initialize.
-        define_method :lazy? do
-          return args[:lazy]
-        end
-
-        # when this option specify true, not raise error in #initialize without value.
-        define_method :optional? do
-          return args[:optional]
-        end
-
         # you can specify :validate option for checking when value is setted.
         # you can use :respond_to as shotcut for specifying { :validate => proc {|val| respond_to?(val, true) } }
         # you can use :isa or :kind_of as shotcut for specifying { :validate => proc {|val| kind_of?(val) } }
@@ -160,10 +139,27 @@ class ClassX
           end
         end
 
-        define_method :set do |val|
-          val = coerce(val)
-          raise ClassX::InvalidAttrArgument unless validate? val
-          @data = val
+        # for extending attribute point.
+        if args[:include]
+          case args[:include]
+          when Array
+            args[:include].each do |mod|
+              self.__send__ :include, mod
+            end
+          else
+            self.__send__ :include, args[:include]
+          end
+        end
+
+        if args[:extend]
+          case args[:extend]
+          when Array
+            args[:extend].each do |mod|
+              self.__send__ :extend, mod
+            end
+          else
+            self.__send__ :extend, args[:extend]
+          end
         end
 
         define_method :inspect do
--- lib/classx/attributes.rb
+++ lib/classx/attributes.rb
@@ -1,19 +1,5 @@
 class ClassX
   module Attributes
-    def define_attribute name, attribute
-      @__attribute_param_of ||= {}
-      if @__attribute_param_of[name]
-        warn "attribute_of :#{name} redefined"
-      end
-      @__attribute_param_of[name] = klass_attribute = ClassX::AttributeFactory.create(attribute)
-      define_method "attribute_of:#{name}" do
-        @__attribute_of ||= {}
-        @__attribute_of[name] ||= klass_attribute.new(self)
-      end
-
-      private "attribute_of:#{name}"
-    end
-
     ATTRIBUTE_REGEX = /\Aattribute_of:(\w+)\z/
 
     def attribute_of
@@ -27,46 +13,60 @@ class ClassX
       hash
     end
 
-    def add_attribute name, attrs={}
-      name = name.to_s
-
-      define_attribute(name, attrs)
-
-      define_method name do
-        attribute_of[name].get
-      end
-
-      define_method "#{name}=" do |val|
-        attribute_of[name].set val
-      end
-
-      @__attribute_param_of ||= {}
-      if @__attribute_param_of[name] 
-        unless @__attribute_param_of[name].config[:writable]
-          private "#{name}="
+    private 
+      def define_attribute name, attribute
+        @__attribute_param_of ||= {}
+        if @__attribute_param_of[name]
+          warn "attribute_of :#{name} redefined"
+        end
+        @__attribute_param_of[name] = klass_attribute = ClassX::AttributeFactory.create(attribute)
+        define_method "attribute_of:#{name}" do
+          @__attribute_of ||= {}
+          @__attribute_of[name] ||= klass_attribute.new(self)
         end
 
-        if @__attribute_param_of[name].config[:handles]
-          case @__attribute_param_of[name].config[:handles]
-          when Hash
-            @__attribute_param_of[name].config[:handles].each do |before, after|
-              define_method before do
-                attribute_of[name].get.__send__ after
+        private "attribute_of:#{name}"
+      end
+
+      def add_attribute name, attrs={}
+        name = name.to_s
+
+        define_attribute(name, attrs)
+
+        define_method name do
+          attribute_of[name].get
+        end
+
+        define_method "#{name}=" do |val|
+          attribute_of[name].set val
+        end
+
+        @__attribute_param_of ||= {}
+        if @__attribute_param_of[name] 
+          unless @__attribute_param_of[name].config[:writable]
+            private "#{name}="
+          end
+
+          if @__attribute_param_of[name].config[:handles]
+            case @__attribute_param_of[name].config[:handles]
+            when Hash
+              @__attribute_param_of[name].config[:handles].each do |before, after|
+                define_method before do
+                  attribute_of[name].get.__send__ after
+                end
               end
-            end
-          when Array
-            @__attribute_param_of[name].config[:handles].each do |meth|
-              define_method meth do
-                attribute_of[name].get.__send__ meth
+            when Array
+              @__attribute_param_of[name].config[:handles].each do |meth|
+                define_method meth do
+                  attribute_of[name].get.__send__ meth
+                end
               end
             end
           end
         end
       end
 
-    end
-
-    alias has add_attribute
+      alias has add_attribute
 
   end
 end
