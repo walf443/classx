28f399333add1d80b76dd3c91a44cb2938d55772
Keiji, Yoshimi <walf443@gmail.com>
2008-08-20 21:58:10 +0900

    added initialize benchmark. classX is TOOOOOOOOOOOOOOOOOOOOOOOOOOO SLOOOOOW now!!!

---
+++ bench/initialize.rb
@@ -0,0 +1,43 @@
+require 'benchmark'
+$LOAD_PATH.unshift(File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib' )))
+require 'classx'
+
+class PointWithClassX < ClassX
+  has :x
+  has :y
+  has :z
+end
+
+class PointWithoutClassX
+  attr_reader :x, :y, :z
+
+  def initialize hash
+    @x = hash[:x]
+    @y = hash[:y]
+    @z = hash[:z]
+  end
+end
+
+COUNT = 1000
+
+Benchmark.bm do |x|
+  x.report 'classx' do
+    COUNT.times do
+      PointWithClassX.new({ :x => rand(10), :y => rand(10), :z => rand(10) })
+    end
+  end
+  x.report 'normal class' do
+    COUNT.times do 
+      PointWithoutClassX.new({ :x => rand(10), :y => rand(10), :z => rand(10) })
+    end
+  end
+end
+
+# On my environment( MacBook1.1 Intel Core Duo 1.83 GHz, 2GB), result is like that. TOOOOO SLOOOW classX!!!!.
+# 
+# ----------------------------------------------------------
+# result on d5619b6533
+#                 user     system      total        real
+# classx        2.130000   0.020000   2.150000 (  2.377707)
+# normal class  0.010000   0.000000   0.010000 (  0.010002)
+
