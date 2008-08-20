93347e055db4f132356f990be82efc332dba3962
Keiji, Yoshimi <walf443@gmail.com>
2008-08-20 23:23:13 +0900

    added benchmark for attribute_set.

---
+++ bench/attribute_set.rb
@@ -0,0 +1,50 @@
+require 'benchmark'
+$LOAD_PATH.unshift(File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib' )))
+require 'classx'
+
+class PointWithClassX < ClassX
+  has :x, :writable => true
+  has :y, :writable => true
+  has :z, :writable => true
+end
+
+class PointWithoutClassX
+  attr_accessor :x, :y, :z
+
+  def initialize hash
+    @x = hash[:x]
+    @y = hash[:y]
+    @z = hash[:z]
+  end
+end
+
+COUNT = 1000
+
+point_with_classx = PointWithClassX.new({ :x => 0, :y => 0, :z => 0 })
+point_without_classx = PointWithoutClassX.new({ :x => 0, :y => 0, :z => 0 })
+
+Benchmark.bm do |x|
+  x.report 'classx' do
+    COUNT.times do
+      point_with_classx.x = rand(10)
+      point_with_classx.y = rand(10)
+      point_with_classx.z = rand(10)
+    end
+  end
+  x.report 'normal class' do
+    COUNT.times do 
+      point_without_classx.x = rand(10)
+      point_without_classx.y = rand(10)
+      point_without_classx.z = rand(10)
+    end
+  end
+end
+
+# On my environment( MacBook1.1 Intel Core Duo 1.83 GHz, 2GB), result is like that. TOOOOO SLOOOW classX!!!!.
+# 
+# ----------------------------------------------------------
+# result on 283903a
+#                 user     system      total        real
+# classx        0.810000   0.020000   0.830000 (  0.902779)
+# normal class  0.000000   0.000000   0.000000 (  0.001401)
+# ----------------------------------------------------------
