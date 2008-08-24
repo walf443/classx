d37368e32770e14a6242bf7eb5e8390a8577e4e0
Keiji, Yoshimi <walf443@gmail.com>
2008-08-24 12:20:07 +0900

    supported defining nested name space.

--- lib/classx/declare.rb
+++ lib/classx/declare.rb
@@ -1,12 +1,23 @@
 module ClassX
   module Declare
-    def classx name, &block
+    def classx name, ctx=_guess_parent_namespace(), &block
       klass = Class.new
       klass.class_eval do
         include(ClassX)
       end
       klass.class_eval &block
-      eval "::#{name.capitalize} = klass"
+      ctx.module_eval do
+        const_set(name.to_s.capitalize, klass)
+      end
     end
+
+    private
+      def _guess_parent_namespace klass=self
+        if self.respond_to? :module_eval
+           klass
+        else
+           klass.class
+        end
+      end
   end
 end
