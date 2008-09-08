e6f03314479f1692e9a04739c2395006ad9cf871
Keiji, Yoshimi <walf443@gmail.com>
2008-09-09 01:50:15 +0900

    added test about inheritance.

--- spec/classx_class_attributes/with_multiple_class_spec.rb
+++ spec/classx_class_attributes/with_multiple_class_spec.rb
@@ -21,5 +21,40 @@ describe ClassX::ClassAttributes do
       end
     end
 
+    describe 'inherit class accessor' do
+      before do
+        @class1 = Class.new
+        @class1.class_eval do
+          extend ClassX::ClassAttributes
+          class_has :x, :default => :default
+        end
+        @class2 = Class.new(@class1)
+      end
+
+      it 'should inherit class attribute' do
+        @class2.class_attribute_of.keys.should == ['x']
+      end
+
+      it "should not be same class attribute's instance" do
+        @class1.class_attribute_of.each do |k1, v1|
+          @class2.class_attribute_of.each do |k2, v2|
+            if k1 == k2
+              v1.should_not == v2
+            end
+          end
+        end
+      end
+
+      it "should not affect subclass changing class attribute value" do
+        @class1.class_eval do
+          self.x = "hoge"
+        end
+        @class2.x.should_not == "hoge"
+      end
+
+      it 'should be able to inherit default value' do
+        @class2.x.should == :default
+      end
+    end
   end
 end
