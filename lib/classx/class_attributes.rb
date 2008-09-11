0c97f016b63fb7853fcc058709ff631be13e2e5e
Keiji, Yoshimi <walf443@gmail.com>
2008-09-12 02:27:43 +0900

    It may be caching is wrong.

--- lib/classx/class_attributes.rb
+++ lib/classx/class_attributes.rb
@@ -85,6 +85,7 @@ module ClassX
           # XXX: Why this can take *args?
           # =>  It's for avoid warnings when you call it without values.
           define_method name do |*vals|
+            name = name.to_s
             if vals == []
               @__class_attribute_data_of ||= {}
               if @__class_attribute_data_of[name]
@@ -118,6 +119,7 @@ module ClassX
           end
 
           define_method "#{name}=" do |val|
+            nam = name.to_s
             attr_instance = nil
             if @__class_attribute_of
               attr_instance = @__class_attribute_of[name]
